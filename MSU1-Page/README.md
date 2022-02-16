02758049ffa6595f40a805a1cd620c8b69ff6e21
Zach P <95437073+picks6@users.noreply.github.com>
2022-02-15 23:39:48 -0500

    Add files via upload

---
+++ MSU1-Page/README.md
@@ -0,0 +1,7 @@
+# MSU1-Page
+
+## What is this?
+A simple webpage to add to my portfolio. Contains details about MSU-1 packs for A Link to the Past Randomizer.
+
+## Deployed Page
+https://picks6.github.io/MSU1-Page/
---
+++ MSU1-Page/assets/js/script.js
@@ -0,0 +1,17 @@
+$(".msu-card-jfg").on('click', function(){
+    $('<a href="https://drive.google.com/file/d/1GGPGq-Kf-e3jRH9XYHWaPwgZn-uVPgOy/view?usp=sharing" target="blank"></a>')[0].click();    
+})
+
+
+$(".msu-card-pss").on('click', function(){
+    $('<a href="https://drive.google.com/file/d/1w4HthST_jSzK2YSFbo3i-23pKai5yecQ/view?usp=sharing" target="blank"></a>')[0].click();    
+})
+
+$(".msu-card-sf64").on('click', function(){
+    $('<a href="https://drive.google.com/file/d/1VHG_MDtewgjPJXiS4gS1oMCKChHDIRRO/view?usp=sharing" target="blank"></a>')[0].click();    
+})
+
+$(".msu-card-pd64").on('click', function(){
+    $('<a href="https://drive.google.com/file/d/1_fvfnv1t_73lrQtv5f4yaFI4OzptwrSl/view?usp=sharing" target="blank"></a>')[0].click();    
+})
+
