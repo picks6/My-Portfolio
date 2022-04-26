9c4de3f17111891e738deb94077fcc5bbd25a475
Zach P <95437073+picks6@users.noreply.github.com>
2022-04-26 17:06:27 -0400

    new js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,3 +9,23 @@ $(".work-card-3").on('click', function(){
 $(".work-card-2").on('click', function(){
     $('<a href="https://picks6.net/Weather-Dashboard/" target="blank"></a>')[0].click();    
 })
+
+$(".work-card-4").on('click', function(){
+    $('<a href="https://picks6.net/Code-Quiz/" target="blank"></a>')[0].click();    
+})
+
+$(".work-card-7").on('click', function(){
+    $('<a href="https://github.com/picks6/README-generator" target="blank"></a>')[0].click();    
+})
+
+$(".work-card-8").on('click', function(){
+    $('<a href="https://blooming-island-47713.herokuapp.com/" target="blank"></a>')[0].click();    
+})
+
+$(".work-card-9").on('click', function(){
+    $('<a href="https://github.com/picks6/EmployeeManager" target="blank"></a>')[0].click();    
+})
+
+$(".work-card-10").on('click', function(){
+    $('<a href="https://github.com/picks6/Ecommerce-Back-End" target="blank"></a>')[0].click();    
+})
