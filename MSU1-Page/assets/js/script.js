a6174661abff17a7496a81f6f249aa3d5c598dc1
Z P <zachpcodes@gmail.com>
2023-02-02 19:23:17 -0500

    Add TO/Winter cards

--- MSU1-Page/assets/js/script.js
+++ MSU1-Page/assets/js/script.js
@@ -1,35 +1,41 @@
 $(".msu-card-jfg").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1GGPGq-Kf-e3jRH9XYHWaPwgZn-uVPgOy/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/1nC4JGuXEOczfoTpXPgF5-biE2L6JC5kD/view?usp=sharing" target="blank"></a>')[0].click();    
 })
 
 
 $(".msu-card-pss").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1w4HthST_jSzK2YSFbo3i-23pKai5yecQ/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/1TC1-6tvNBe_GBmJiteth4roM7ZYE_xg_/view?usp=sharing" target="blank"></a>')[0].click();    
 })
 
 $(".msu-card-sf64").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1VHG_MDtewgjPJXiS4gS1oMCKChHDIRRO/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/1reH6ACqNtWgEbqTJX0N-R16TwbtE6LSV/view?usp=sharing" target="blank"></a>')[0].click();    
 })
 
 $(".msu-card-pd64").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1_fvfnv1t_73lrQtv5f4yaFI4OzptwrSl/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/13Hddpk9W6hNlDguVtLYv79X0tyuKQj1b/view?usp=sharing" target="blank"></a>')[0].click();    
 })
 
 $(".msu-card-wr64").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1tatuSAoPNtVRr_4TY0sGR3pej71lvtBw/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/1YOOCbNjMqwlGcE9byPdJRyQca8Rtg5i4/view?usp=sharing" target="blank"></a>')[0].click();    
 })
 
 $(".msu-card-ts").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1GTIqzmTxs5LatiTy8iCgbCGTMS5MGqAp/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/1TQKh_D33jqV105WKmnn6dNVnTv2_AgkD/view?usp=sharing" target="blank"></a>')[0].click();    
 })
 
 $(".msu-card-q64").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1dqhDNgWpW_-39BtYf_Yk2eKKrU6fPdHO/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/1lV4guWq1oA6zwb5jKiseaShp9-zRMt-s/view?usp=sharing" target="blank"></a>')[0].click();    
 })
 
 $(".msu-card-twc").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1kr0ssU73_542eleWyIgK-pKZs7omT0uo/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/1iRpDjX-5ziWu_frsvGCnqTARLt-u0sZ9/view?usp=sharing" target="blank"></a>')[0].click();    
 })
 $(".msu-card-lmtrilogy").on('click', function(){
-    $('<a href="https://drive.google.com/file/d/1md55dFcokXZAKhEEloLjSbSu6qwxbDvj/view?usp=sharing" target="blank"></a>')[0].click();    
+    $('<a href="https://drive.google.com/file/d/1C3HU4EZYLvEuVdzTuyTDIRsiQaKw6xwj/view?usp=sharing" target="blank"></a>')[0].click();    
+})
+(".msu-card-toreborn").on('click', function () {
+        $('<a href="https://drive.google.com/file/d/1m0bhkS6vcDFbS5lAT-OXRWh_2cseS-6d/view?usp=sharing" target="blank"></a>')[0].click();
+})
+(".msu-card-winter").on('click', function () {
+    $('<a href="https://drive.google.com/file/d/106C5dvUxtztlvOMiSKPS_1mmT_mT4QZ3/view?usp=sharing" target="blank"></a>')[0].click();
 })
